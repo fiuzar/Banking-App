f4833f989c71f2ebdf2ca25eef25d001edbc6674
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2026-01-20 14:18:42 +0100

    version 0.0.9 sub

--- src/components/layout/register-form.jsx
+++ src/components/layout/register-form.jsx
@@ -96,6 +96,10 @@ export function RegisterForm({ className, ...props }) {
                             </div>
                 </CardContent>
             </Card>
+            <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+                By clicking continue, you agree to our <Link href="/terms">Terms of Service</Link>{" "}
+                and <Link href="/privacy-policy">Privacy Policy</Link>.
+            </div>
         </div>
     )
 }
